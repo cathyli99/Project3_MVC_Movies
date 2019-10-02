0fa293d1fde1c7232fd1bdc345f8a1a48a99ca2d
Cathy Li <cathyli99nz@gmail.com>
2019-10-03 07:01:56 +1000

    initial upload

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Get: /HelloWorkd/Welcome/
+        public IActionResult Welcome(int ID, string name, int numTimes = 1)
+        {
+            //return HtmlEncoder.Default.Encode($"Hello {name}, numTimes = {numTimes}, ID: {ID}");
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    } 
+}
